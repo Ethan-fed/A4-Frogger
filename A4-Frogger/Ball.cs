a18e84e036f61e6afe12f8d182f117325fea1c91
karjon7 <80655086+karjon7@users.noreply.github.com>
2024-11-30 14:39:24 -0500

    Declare variables and create constructor

--- A4-Frogger/Ball.cs
+++ A4-Frogger/Ball.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,17 @@ namespace A4_Frogger
 {
     public class Ball
     {
+        Vector2 position;
+        Vector2 velocity;
 
+        int size = 25;
+        
+        
+        public Ball(Vector2 startingPosition, Vector2 startingVelocity, int size = 25) 
+        {
+            position = startingPosition;
+            velocity = startingVelocity;
+            this.size = size;
+        }
     }
 }
