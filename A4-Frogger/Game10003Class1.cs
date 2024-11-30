d40c0fe15236e5717a7defbe264c004e2988febf
Olwyn Roberts <000852881@mohawkcollege.ca>
2024-11-29 23:08:49 -0500

    create brick class
    added brick class, set up basic brick draw command

---
+++ A4-Frogger/Game10003Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+namespace Game10003
+{
+    public class Bricks
+    {
+
+        public Color BrickColor;
+        public int BrickWidth;
+        public int BrickHeight;
+        public float BrickRadius;
+        public Vector2 BrickPosition;
+
+        public Bricks() 
+        {
+            BrickColor = Color.White;
+
+        }
+        public void Render()
+        {
+            Draw.FillColor = BrickColor;
+            Draw.LineColor = Color.Black;
+            Draw.LineSize = 2;
+        }
+    }
+}
